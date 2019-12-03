40a41edeafaabb0928e6fd70dc47b25d4dd71723
Kate <katarzynakolata3@gmail.com>
2019-12-03 12:03:36 +0100

    Test Pogoda w Warszawie

---
+++ Project1/GoogleTestscs.cs
@@ -0,0 +1,40 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace Project1
+{
+    public class GoogleTestscs: System.IDisposable
+    {
+        IWebDriver browser;
+
+        public GoogleTestscs()
+        {
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void CanGoogleWeatherForWarsaw()
+        {
+            browser.Navigate().GoToUrl("https://google.com");
+            var queryBox = browser.FindElement(By.Name("q"));
+            queryBox.Click();
+            queryBox.SendKeys("pogoda warszawa");
+            queryBox.Submit();
+
+            var result = browser.FindElement(By.Id("wob_tm"));
+
+            Assert.Equal("3", result.Text);
+
+            
+        }
+
+        public void Dispose()
+        {
+            browser.Quit();
+        }
+    }
+}
